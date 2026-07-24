f072acb7f24f980969df1bd0589d96024694b575
arafatai388 <arafat.cseai@gmail.com>
2026-07-24 11:02:08 +0600

    Learn JavaScript object basics and object literals

---
+++ object.js
@@ -0,0 +1,23 @@
+const sunglass = {
+    brand: "Ray Ban",
+    color: "White",
+    price: 120
+};
+
+const phone = {
+    brand: "Apple",
+    version: 17,
+    isThreeCamera: false,
+    secondHand: false,
+    appsInstalled: [
+        "Facebook",
+        "ChatGPT",
+        "Neptune"
+    ]
+};
+
+console.log("Sunglass:");
+console.log(sunglass);
+
+console.log("Phone:");
+console.log(phone);
